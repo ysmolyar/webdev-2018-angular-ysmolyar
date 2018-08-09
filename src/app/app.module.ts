253962bf502e97b6a0454ed6041824ab45537df5
ysmolyar <yonismolyar1@gmail.com>
2018-08-09 00:37:21 -0400

    made forms for login page, sending data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { WhiteBoardComponent } from './white-board/white-board.component';
 import {CourseServiceClient} from './services/course.service.client';
 import { CourseNavigatorComponent } from './course-navigator/course-navigator.component';
+
 import { routing } from './app.routing';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
@@ -21,7 +23,8 @@ import { ProfileComponent } from './profile/profile.component';
   ],
   imports: [
     BrowserModule,
-    routing
+    routing,
+    FormsModule
   ],
   providers: [
     CourseServiceClient
