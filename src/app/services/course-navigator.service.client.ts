9ce9e1758ef86e7b21a03f0780d6b9cf80a499c6
ysmolyar <yonismolyar1@gmail.com>
2018-08-09 13:54:43 -0400

    made simple course navigator service

---
+++ src/app/services/course-navigator.service.client.ts
@@ -0,0 +1,13 @@
+import {Injectable} from '@angular/core';
+
+@Injectable()
+export class CourseNavigatorServiceClient {
+  findAllCourses() {
+    return fetch('http://localhost:8080/api/course')
+      .then(response => response.json())
+  }
+  findAllModulesForGivenCourse(courseId) {
+    return fetch('http://localhost:8080/api/course/' + courseId + '/module')
+      .then(response => response.json());
+  }
+}
