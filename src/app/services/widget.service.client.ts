2a159837b7fa1b379c0fe44b682f24669af6c988
ysmolyar <yonismolyar1@gmail.com>
2018-08-10 13:45:34 -0400

    made simple widgets, need to go fix widgets in java backend

---
+++ src/app/services/widget.service.client.ts
@@ -0,0 +1,6 @@
+export class WidgetServiceClient {
+  findWidgetsForLesson(lessonId) {
+    return fetch('https://beastmode-webdev-ysmolyar.herokuapp.com/api/widget')
+      .then(response => response.json());
+  }
+}
